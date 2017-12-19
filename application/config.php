248baf07ab66f8fdee01d7020c46d755ce8bad03
suxiaoxin <654108442@qq.com>
2017-12-19 16:01:50 +0800

    修复CSS，JS文件引入路径问题

--- application/config.php
+++ application/config.php
@@ -141,9 +141,7 @@ return [
     ],
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [
-        '__static__' => str_replace($_SERVER['DOCUMENT_ROOT'], '', str_replace("\\", "/", getcwd())).'/static',
-    ],
+    'view_replace_str'       => [],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
     'dispatch_error_tmpl'    => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
